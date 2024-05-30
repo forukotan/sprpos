d77fa7e96e4bd488324e133b3046c6430f4b871f
forukotan <forukotan1@gmail.com>
2024-05-30 18:11:12 -0500

    lets get the party started in here

---
+++ src/main/java/com/imef/POSTSPRING/PostspringApplication.java
@@ -0,0 +1,13 @@
+package com.imef.POSTSPRING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imef/POSTSPRING/PostspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imef.POSTSPRING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
